2f269da2916f5098159b2caff444c638b52a3e10
antoHero <veeqanto@gmail.com>
2023-03-13 21:17:38 +0100

    add border to bottom of active link

--- src/components/shared/PageHeader.vue
+++ src/components/shared/PageHeader.vue
@@ -1,14 +1,29 @@
 <template>
   <q-header elevated class="bg-white text-grey-10">
-    <q-toolbar class="q-pa-md row no-wrap justify-center items-center">
-      <!-- <q-btn dense flat round icon="menu" @click="showMenu = !showMenu" /> -->
+    <q-toolbar
+      class="row no-wrap justify-center items-center"
+      style="bottom: 0"
+    >
       <q-text-h2
         class="row items-center"
         style="color: #474498; font-size: 18px"
         >Paypay.</q-text-h2
       >
       <q-list class="q-ma-none q-ml-lg q-py-none row items-center">
-        <q-item :to="link.to" exact v-for="(link, idx) in links" :key="idx">
+        <q-item
+          style="
+            border-bottom: 3px solid transparent;
+            padding: 20px;
+            height: 70px;
+          "
+          :to="link.to"
+          exact
+          v-for="(link, idx) in links"
+          :key="idx"
+          :class="
+            $route.matched.some(({ path }) => path === link.to) ? 'active' : ''
+          "
+        >
           <q-item-section avatar>
             <q-icon :name="link.icon" />
           </q-item-section>
@@ -49,3 +64,10 @@ export default defineComponent({
   },
 });
 </script>
+
+<style>
+.active {
+  border-bottom: 3px solid #63657f !important;
+  color: #423dbe !important;
+}
+</style>
