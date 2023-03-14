0f8c1888dc619b1fd371ee1f60aef27aaf2a3027
antoHero <veeqanto@gmail.com>
2023-03-14 10:14:27 +0100

    make table responsive and move card and items to separate components

---
+++ src/components/InvoiceItems.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="row items-start justify-between">
+    <div class="">
+      <p class="m-0"><b>Item Detail</b></p>
+      <p class="text-grey">Type Item for Hours item.</p>
+    </div>
+    <q-space />
+    <q-btn icon="tune" flat size="md" label="Customize" class="customize-btn" />
+  </div>
+  <div class="table-container">
+    <table>
+      <thead>
+        <tr class="header-row">
+          <th
+            class="text-grey text-capitalize"
+            v-for="header in props.tableHeaders"
+            :key="header.id"
+            scope="col"
+            :width="header.id === 1 ? '50%' : 'auto'"
+          >
+            {{ header.name }}
+          </th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="(item, idx) in props.rows" :key="idx">
+          <td data-label="item Name">
+            <div class="mb-2 table-items">
+              {{ item.name }}
+            </div>
+          </td>
+          <td data-label="Hours" class="">
+            <div class="table-items">
+              {{ item.hours }}
+            </div>
+          </td>
+
+          <td data-label="Rate/hr">
+            <div class="table-items">
+              {{ item.rate }}
+            </div>
+          </td>
+          <td data-label="Tax">
+            <div class="table-items">
+              {{ item.tax }}
+            </div>
+          </td>
+          <td data-label="Line Total">
+            <div class="table-items">
+              {{ item.line_total }}
+            </div>
+          </td>
+          <td data-label="Action">
+            <q-btn round color="primary" icon="add" class="action-btn" />
+          </td>
+        </tr>
+        <tr>
+          <td data-label="item Name">
+            <div class="mb-2 table-items">Description</div>
+          </td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  tableHeaders: Array,
+  rows: Array,
+});
+</script>
+
+<style lang="scss" scoped>
+.customize-btn {
+  border: 1px solid #dbeafe;
+  background-color: #dbeafe;
+  color: #1e40af;
+  border-radius: 10px;
+}
+
+.table-container {
+  overflow-x: auto;
+}
+
+.table-items {
+  border: 1px solid #e1e4e7;
+  border-radius: 10px;
+  padding: 10px;
+  margin: 0 2px 0 2px;
+  background: #fcfcfc;
+}
+
+.action-btn {
+  background-color: #1d4ed8 !important;
+  color: #fff;
+}
+
+table {
+  border-collapse: collapse;
+  margin: 1rem 0;
+  padding: 0;
+  width: 100%;
+  tr {
+    padding: 0.35em;
+  }
+  th,
+  td {
+    padding: 0.625em;
+    text-align: start;
+    vertical-align: baseline;
+  }
+  th {
+    font-size: 0.85em;
+    border-bottom: 1px solid #ddd;
+    letter-spacing: 0.1em;
+    text-transform: uppercase;
+    text-align: center;
+  }
+}
+</style>
--- src/pages/IndexPage.vue
+++ src/pages/IndexPage.vue
@@ -15,91 +15,14 @@
         <q-card-section>
           <billing-info></billing-info>
 
-          <div class="row items-start justify-between q-pa-lg blue-gradient">
-            <div class="column vertical-middle">
-              <p><b>Invoice Number</b></p>
-              <p>INV-2022-010</p>
-              <p>Issued Date: 11 Jan 2022</p>
-              <p>Due Date: 18 Jan 2022</p>
-            </div>
-            <q-space />
-            <div class="column text-right vertical-middle">
-              <p><b>Billed To</b></p>
-              <p>Zaky Grizzly</p>
-              <p>Monlight Agency LTD</p>
-              <p>East Java, Indonesia</p>
-            </div>
-          </div>
+          <invoice-card></invoice-card>
 
           <div class="q-pa-lg">
-            <div class="row items-start justify-between">
-              <div class="">
-                <p class="m-0"><b>Item Detail</b></p>
-                <p class="text-grey">Type Item for Hours item.</p>
-              </div>
-              <q-space />
-              <q-btn
-                icon="tune"
-                flat
-                size="md"
-                label="Customize"
-                class="customize-btn"
-              />
-            </div>
-
-            <table>
-              <thead>
-                <tr>
-                  <th
-                    class="text-grey text-capitalize"
-                    v-for="header in tableHeaders"
-                    :key="header.id"
-                    scope="col"
-                    :width="header.id === 1 ? '50%' : 'auto'"
-                  >
-                    {{ header.name }}
-                  </th>
-                </tr>
-              </thead>
-              <tbody>
-                <tr v-for="(item, idx) in rows" :key="idx">
-                  <td data-label="item Name">
-                    <div class="mb-2 table-items">
-                      {{ item.name }}
-                    </div>
-                  </td>
-                  <td data-label="Hours" class="">
-                    <div class="table-items">
-                      {{ item.hours }}
-                    </div>
-                  </td>
-
-                  <td data-label="Rate/hr">
-                    <div class="table-items">
-                      {{ item.rate }}
-                    </div>
-                  </td>
-                  <td data-label="Tax">
-                    <div class="table-items">
-                      {{ item.tax }}
-                    </div>
-                  </td>
-                  <td data-label="Line Total">
-                    <div class="table-items">
-                      {{ item.line_total }}
-                    </div>
-                  </td>
-                  <td data-label="Action">
-                    <q-btn round color="primary" icon="add" />
-                  </td>
-                </tr>
-                <tr>
-                  <td data-label="item Name">
-                    <div class="mb-2 table-items">Description</div>
-                  </td>
-                </tr>
-              </tbody>
-            </table>
+            <InvoiceItems
+              :tableHeaders="tableHeaders"
+              :rows="rows"
+              :key="invoiceItemsKey"
+            />
             <q-separator />
             <div class="row justify-center items-center q-pt-md">
               <div class="row w-100">
@@ -180,6 +103,8 @@
 <script>
 import { defineComponent, ref } from "vue";
 import BillingInfo from "../components/BillingInfo.vue";
+import InvoiceCard from "../components/InvoiceCard.vue";
+import InvoiceItems from "src/components/InvoiceItems.vue";
 import { dollar } from "../utils/helper";
 const items = [
   {
@@ -207,19 +132,6 @@ const items = [
     name: "",
   },
 ];
-const columns = [
-  {
-    name: "name",
-    required: true,
-    label: "Item Name",
-    align: "left",
-    field: (row) => row.name,
-  },
-  { name: "hours", align: "center", label: "Hours", field: "hours" },
-  { name: "rate", label: "Rate/Hr", field: "rate" },
-  { name: "tax", label: "Tax", field: "tax" },
-  { name: "line_total", label: "Line Total", field: "line_total" },
-];
 const rows = [
   {
     name: "Payment Project - Monlight Mobile Design",
@@ -233,136 +145,26 @@ export default defineComponent({
   name: "IndexPage",
   components: {
     BillingInfo,
+    InvoiceCard,
+    InvoiceItems,
   },
   setup() {
-    const formData = ref({
-      itemName: "Payment Project - Monlight Mobile Design",
-      itemDescription: "Description",
-      hours: 120,
-      rate: 40,
-      tax: 30,
-    });
+    const invoiceItemsKey = ref(0);
 
-    const lineTotal = ref(
-      formData.value.hours * formData.value.rate + formData.value.tax
-    );
+    invoiceItemsKey.value += 1;
 
     return {
-      formData,
       tableHeaders: items,
-      lineTotal,
-      columns,
       rows,
       dollar,
+      invoiceItemsKey,
     };
   },
 });
 </script>
 
 <style lang="scss" scoped>
-.blue-gradient {
-  color: #fff;
-  border-radius: 25px;
-  background: -webkit-linear-gradient(110deg, #3f3f9f 40%, rgba(0, 0, 0, 0) 30%),
-    -webkit-radial-gradient(farthest-corner at 0% 0%, #4d4ba2 70%, #6e70ad 70%);
-  background: -o-linear-gradient(110deg, #3f3f9f 40%, rgba(0, 0, 0, 0) 30%),
-    -o-radial-gradient(farthest-corner at 0% 0%, #4d4ba2 70%, #6e70ad 70%);
-  background: -moz-linear-gradient(110deg, #3f3f9f 40%, rgba(0, 0, 0, 0) 30%),
-    -moz-radial-gradient(farthest-corner at 0% 0%, #4d4ba2 70%, #6e70ad 70%);
-  background: linear-gradient(110deg, #3f3f9f 40%, rgba(0, 0, 0, 0) 30%),
-    radial-gradient(farthest-corner at 0% 0%, #4d4ba2 70%, #6e70ad 70%);
-}
-
 .page-container {
   padding-top: 70px;
-}
-
-.customize-btn {
-  background-color: #eceff9;
-  color: #9598c9;
-  border-radius: 10px;
-}
-
-.table-items {
-  border: 1px solid #e1e4e7;
-  border-radius: 10px;
-  padding: 10px;
-  margin: 0 2px 0 2px;
-  background: #fcfcfc;
-}
-
-table {
-  border-collapse: collapse;
-  margin: 1rem 0;
-  padding: 0;
-  width: 100%;
-  table-layout: fixed;
-  tr {
-    padding: 0.35em;
-  }
-  th,
-  td {
-    padding: 0.625em;
-    text-align: start;
-    vertical-align: baseline;
-  }
-  th {
-    font-size: 0.85em;
-    border-bottom: 1px solid #ddd;
-    letter-spacing: 0.1em;
-    text-transform: uppercase;
-    text-align: center;
-  }
-}
-
-@media screen and (max-width: 600px) {
-  table {
-    border: 0;
-  }
-  table caption {
-    font-size: 1.3em;
-  }
-  table thead {
-    border: none;
-    clip: rect(0 0 0 0);
-    height: 1px;
-    margin: -1px;
-    overflow: hidden;
-    padding: 0;
-    position: absolute;
-    width: 1px;
-  }
-  table tr {
-    border-bottom: 3px solid #ddd;
-    display: flex;
-    flex-direction: column;
-  }
-  table td {
-    border-bottom: 1px solid #ddd;
-    display: block;
-    font-size: 0.8em;
-  }
-  table td::before {
-    content: attr(data-label);
-    float: left;
-    font-weight: bold;
-    display: block;
-    text-transform: uppercase;
-  }
-  table td:last-child {
-    border-bottom: 0;
-  }
-}
-// Media queries
-@media (max-width: 768px) {
-  table {
-    td {
-      padding: 1rem 0.625rem;
-      &::before {
-        float: none;
-        margin-bottom: 1rem;
-      }
-    }
-  }
 }
 </style>
