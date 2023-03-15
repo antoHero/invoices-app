8687d132a814b7591c2b5272a8b77f8609a8d92e
antoHero <veeqanto@gmail.com>
2023-03-15 15:39:08 +0100

    Add new pages and create mobile menu

--- src/utils/helper.js
+++ src/utils/helper.js
@@ -1,5 +1,25 @@
+import { ref, onMounted, onUnmounted } from "vue";
+
 export const dollar = new Intl.NumberFormat("en-US", {
   style: "currency",
   currency: "USD",
   minimumFractionDigits: 2,
 });
+
+export const domSize = () => {
+  const width = ref(0);
+  const height = ref(0);
+
+  function update() {
+    width.value = window.innerWidth;
+    height.value = window.innerHeight;
+  }
+
+  onMounted(() => {
+    update();
+    window.addEventListener("resize", update);
+  });
+  onUnmounted(() => window.removeEventListener("resize", update));
+
+  return { width, height };
+};
